b0b0064f211e5ff5f59cd93a546f833001fcb8be
Geoffrey Parquet <geoffrey.parquet@viacesi.fr>
2022-11-05 17:06:13 +0100

    feat(authentication): ajout de l'api de l'authentification

--- server.js
+++ server.js
@@ -1,42 +1,49 @@
 var express = require('express'),
-    cors = require("cors"),
-    app = express(),
-    axios = require('axios');
-
-const { Octokit } = require("@octokit/core");
+	cors = require('cors'),
+	app = express(),
+	axios = require('axios');
 
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const githubUsername = process.env.USER_NAME;
-const githubToken = process.env.TOKEN;
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
 
 app.use(cors());
 
-app.get("/api/user", (req, res) => {
-    axios({
-        method: "get",
-        url: `https://api.github.com/users/${githubUsername}`,
-        headers: {
-            Authorization: `Bearer ${githubToken}`,
-            "Content-Type": "application/json",
-            "Accept": "application/vnd.github.mercy-preview+json" // MUST ADD TO INCLUDE TOPICS
-        }
-    }).then(response => {
-        res.send(response.data);
-    }).catch(err => {
-        res.send(err);
-    });
+app.get('/auth', (req, res) => {
+	res.redirect(
+		`https://github.com/login/oauth/authorize?client_id=${clientId}&scope=read:user,repos,user:email,read:org`
+	);
 });
 
-
-
+app.get('/callback', (req, res) => {
+	axios
+		.post(
+			'https://github.com/login/oauth/access_token',
+			{
+				client_id: clientId,
+				client_secret: clientSecret,
+				code: req.query.code,
+			},
+			{
+				headers: {
+					Accept: 'application/json',
+				},
+			}
+		)
+		.then((result) => {
+			console.log(JSON.stringify(result.data));
+			res.send('you are authorized ' + result.data.access_token);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
 
 const PORT = process.env.PORT || 3000;
 module.exports = app.listen(PORT, () => {
-    console.log('Server running on port %d', PORT);
-    console.log('Mon nom de repos git hub est :' + githubUsername);
-    console.log('Mon token est ..... c\'est secret :) ');
-})
+	console.log('Server running on port %d', PORT);
+});
